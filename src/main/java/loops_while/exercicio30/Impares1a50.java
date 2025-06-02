58e92d4aefcca066874453a36c06dc0b5bace426
Bernard De Freitas <bernard30101994@gmail.com>
2025-06-02 13:08:29 -0300

    Adicionar programas de lógica em Java: Impares1a50 e ValidacaoSenha

--- src/main/java/loops_while/exercicio30/Impares1a50.java
+++ src/main/java/loops_while/exercicio30/Impares1a50.java
@@ -1,4 +1,29 @@
+/*
+Exercício 30: Crie um programa que imprima números ímpares de 1 a N.
+*/
+
 package main.java.loops_while.exercicio30;
 
+import java.util.Scanner;
+
 public class Impares1a50 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número para");
+
+        int numero = scanner.nextInt();
+
+        int contador = 0;
+
+        while(contador < numero) {
+            contador++;
+
+            if(contador % 2 != 0) {
+                System.out.println(contador);
+            }
+        }
+        scanner.close();
+    }
 }
--- src/main/java/loops_while/exercicio31/ValidacaoSenha.java
+++ src/main/java/loops_while/exercicio31/ValidacaoSenha.java
@@ -1,4 +1,31 @@
+/*
+Exercício 31: Desenvolva um programa que valide uma senha inserida pelo usuário.usuário. A senha correta é "secret123".
+Enquanto o usuário não digitar a senha correta, o programa continuará solicitando-a.
+*/
+
 package main.java.loops_while.exercicio31;
 
+import java.util.Scanner;
+
 public class ValidacaoSenha {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final String senhaCorreta = "secret123";
+
+        System.out.println("Digite a senha: ");
+        String senhaInformada = scanner .nextLine();
+
+        while (!senhaInformada.equals(senhaCorreta)) {
+            System.out.println("Senha incorreta. Tente novamente.");
+            System.out.println("Digite a senha: ");
+            senhaInformada = scanner.nextLine();
+        }
+
+        if (senhaInformada.equals(senhaCorreta)) {
+            System.out.println("Senha correta! Acesso concedido.");
+        }
+
+    }
 }
