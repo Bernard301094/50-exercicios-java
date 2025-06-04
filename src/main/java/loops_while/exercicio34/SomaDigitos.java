ef8853ea642ff1b75f5731481e1a1eb3609c7a0a
Bernard De Freitas <bernard30101994@gmail.com>
2025-06-03 21:08:41 -0300

    Adicionar programas de lógica em Java: EstatisticasNumeros, Palindromo e SomaDigitos

--- src/main/java/loops_while/exercicio34/SomaDigitos.java
+++ src/main/java/loops_while/exercicio34/SomaDigitos.java
@@ -1,4 +1,33 @@
+/*
+Exercício 34: Desenvolva um programa que some os dígitos de um número inteiro positivo inserido pelo usuário.
+Por exemplo, se o número for 123, a soma será 1+2+3=6.
+*/
 package main.java.loops_while.exercicio34;
 
+import java.util.Scanner;
+
 public class SomaDigitos {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro positivo: ");
+        int numero = scanner.nextInt();
+
+        if (numero < 0) {
+            System.out.println("Número inválido. Deve ser positivo.");
+            return;
+        }
+
+        int soma = 0;
+        int temp = numero;
+
+        while (temp > 0) {
+            soma += temp % 10;
+            temp /= 10;
+        }
+
+        System.out.println("Soma dos dígitos de " + numero + " é: " + soma);
+        scanner.close();
+    }
 }
--- src/main/java/loops_while/exercicio35/EstatisticasNumeros.java
+++ src/main/java/loops_while/exercicio35/EstatisticasNumeros.java
@@ -1,4 +1,43 @@
+/*
+Exercício 35: Escreva um programa que leia números inteiros até que um 0 seja inserido.
+No final, ele deve mostrar quantos números foram inseridos (sem contar o 0), a soma total e a média.
+*/
+
 package main.java.loops_while.exercicio35;
 
+import java.util.Scanner;
+
 public class EstatisticasNumeros {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro (0 para sair):");
+        int numero = scanner.nextInt();
+        int numeroAnterior;
+        int contador = 0;
+        int soma = 0;
+        int media = 0;
+
+        while (numero != 0) {
+            System.out.println("Você digitou: " + numero);
+            contador++;
+            numeroAnterior = numero;
+            soma += numero;
+            media = soma / contador;
+
+            if (numero > 0) {
+                System.out.println("Digite outro número inteiro (0 para sair):");
+                numero = scanner.nextInt();
+            } else {
+                System.out.println("Número inválido. Por favor, digite um número inteiro positivo ou 0 para sair.");
+                numero = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Total de números inseridos: " + contador);
+        System.out.println("Soma total: " + soma);
+        System.out.println("Média: " + media);
+        scanner.close();
+    }
 }
