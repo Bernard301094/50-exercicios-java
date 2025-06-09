bcf7bf7217dc749238b6cb127a1be2014444c5a6
Bernard De Freitas <bernard30101994@gmail.com>
2025-06-09 12:33:06 -0300

    Adicionar programas de lógica em Java: Contagem Regressiva e Contar Vogais

--- src/main/java/loops_while/exercicio39/ContarVogaisInput.java
+++ src/main/java/loops_while/exercicio39/ContarVogaisInput.java
@@ -1,4 +1,34 @@
+/*
+ Exercício 39: Crie um programa que leia caracteres do teclado até que o usuário
+ digite um ponto ('.'). Conte quantas vogais foram inseridas.
+ */
+
 package main.java.loops_while.exercicio39;
 
+import java.util.Scanner;
+
 public class ContarVogaisInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int contador = 0;
+        String input;
+
+        while (true) {
+            System.out.print("Digite um texto (ou '.' para sair): ");
+            input = scanner.nextLine();
+
+            if (input.equals(".")) {
+                break;
+            }
+
+            for (int i = 0; i < input.length(); i++) {
+                char caractere = input.charAt(i);
+                if ("aeiouAEIOU".indexOf(caractere) != -1) {
+                    contador++;
+                }
+            }
+        }
+        System.out.println("Total de vogais digitadas: " + contador);
+        scanner.close();
+    }
 }
--- src/main/java/loops_while/exercicio40/ContagemRegressiva.java
+++ src/main/java/loops_while/exercicio40/ContagemRegressiva.java
@@ -1,4 +1,23 @@
+/*
+ Exercício 40: Desenvolva um programa que simule uma contagem regressiva a partir
+ de um número N informado pelo usuário até 0, exibindo cada número na tela.
+*/
+
 package main.java.loops_while.exercicio40;
 
+import java.util.Scanner;
+
 public class ContagemRegressiva {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número para iniciar a contagem regressiva: ");
+        int numero = scanner.nextInt();
+
+        while(numero >= 0) {
+            System.out.println(numero);
+            numero--;
+        }
+    }
 }
